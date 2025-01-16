ba562cd69bab224007cc5338301f870e19ac21d2
constance khalumba <constancekhalumba@gmail.com>
2025-01-16 10:00:26 +0300

    first commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,22 @@
-const Button = () => {
+import { forwardRef } from "react"
+
+interface ButtonProps
+extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+  className,
+  children,
+  disabled,
+  type = "button",
+  ...props
+}, ref) => {
     return (
-        <div>
-           Button 
-        </div>
+        <button>
+            
+        </button>
     )
-}
+})
+
+Button.displayName = "Button";
 
 export default Button;
