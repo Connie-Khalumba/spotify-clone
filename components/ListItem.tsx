d1895ebc41197c3a0e5549350f79de87346789a2
constance khalumba <constancekhalumba@gmail.com>
2025-01-20 16:36:46 +0300

    first commits

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
@@ -50,9 +51,28 @@ const ListItem: React.FC<ListItemProps> = ({
               />
               
             </div>
-            <p>
+            <p className="font-medium truncate py-5">
                 {name}
             </p>
+            <div
+            className="
+             absolute
+             transition
+             opacity-0
+             rounded-full
+             flex
+             items-center
+             justify-center
+             bg-green-500
+             p-4
+             drop-shadow-mdd
+             right-5
+             group-hover:opacity-100
+             hover:scale-110
+            ">
+                <FaPlay className="text-black"/>
+
+            </div>
 
         </button>
     )
