32ece5464bdd8b6ef3f67fe631dc046d15bf8e9e
constance khalumba <constancekhalumba@gmail.com>
2025-01-23 14:42:33 +0300

    second commits

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Database } from "@/types_db";
+import { ReactNode } from "react";
+
+interface SuperbaseProviderProps {
+   children: ReactNode; 
+};
+
+const SuperbaseProvider: React.FC<SuperbaseProviderProps> = ({
+    children
+}) => {
+    
+}
