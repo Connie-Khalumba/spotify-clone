da87ca80e86daeac3957cbb9b09839760a4c05f5
constance khalumba <constancekhalumba@gmail.com>
2025-01-24 14:09:40 +0300

    third commits

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,8 +1,9 @@
 import { User } from "@supabase/auth-helpers-nextjs";
-import { createContext } from "react";
+import { useSessionContext, useUser as useSupaUser} from "@supabase/auth-helpers-react";
+import { createContext, useState } from "react";
 
 import { Subscription, UserDetails } from "./types";
-import { useSessionContext } from "@supabase/auth-helpers-react";
+
 
 type UserContextType = {
     accessToken: string | null;
@@ -21,5 +22,29 @@ export interface Props {
 }
 
 export const MyUserContextProvider = (props: Props) =>{
-    const {} = useSessionContext();
+    const {
+        session,
+        isLoading: isLandingUser,
+        supabaseClient: supabase
+    } = useSessionContext();
+    const user = useSupaUser();
+    const accessToken = session?.access_token ?? null;
+    const [isLoadingData, setIsLoadingData] = useState(false);
+    const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+    const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+    const getUserDetails = () => supabase.from('users').select('*').single();
+    const getSubscription = () => supabase
+    .from('subscriptions')
+    .select('*, prices(*, products(*))')
+    .in('status', ['trialing', 'active'])
+    .single();
+
+    useEffect(() => {
+        if (user && !isLoadingData && !userDetails && !subscription) {
+            setIsLoadingData(true);
+
+            Promise.allSettled([getUserDetails, getSubscription])
+        }         
+    }, [])
 }
